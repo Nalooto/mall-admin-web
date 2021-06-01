a2ece3b4b65703695ea054e648140b38426c3d99
Nalooto <973022448@qq.com>
2021-06-01 20:30:01 +0800

    bugfix

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -212,21 +212,21 @@
   const DATA_FROM_BACKEND = {
     columns: ['date', 'orderCount','orderAmount'],
     rows: [
-      {date: '2018-11-01', orderCount: 10, orderAmount: 1093},
-      {date: '2018-11-02', orderCount: 20, orderAmount: 2230},
-      {date: '2018-11-03', orderCount: 33, orderAmount: 3623},
-      {date: '2018-11-04', orderCount: 50, orderAmount: 6423},
-      {date: '2018-11-05', orderCount: 80, orderAmount: 8492},
-      {date: '2018-11-06', orderCount: 60, orderAmount: 6293},
-      {date: '2018-11-07', orderCount: 20, orderAmount: 2293},
-      {date: '2018-11-08', orderCount: 60, orderAmount: 6293},
-      {date: '2018-11-09', orderCount: 50, orderAmount: 5293},
-      {date: '2018-11-10', orderCount: 30, orderAmount: 3293},
-      {date: '2018-11-11', orderCount: 20, orderAmount: 2293},
-      {date: '2018-11-12', orderCount: 80, orderAmount: 8293},
-      {date: '2018-11-13', orderCount: 100, orderAmount: 10293},
-      {date: '2018-11-14', orderCount: 10, orderAmount: 1293},
-      {date: '2018-11-15', orderCount: 40, orderAmount: 4293}
+      {date: '2021-5-01', orderCount: 10, orderAmount: 1093},
+      {date: '2021-5-02', orderCount: 20, orderAmount: 2230},
+      {date: '2021-5-03', orderCount: 33, orderAmount: 3623},
+      {date: '2021-5-04', orderCount: 50, orderAmount: 6423},
+      {date: '2021-5-05', orderCount: 80, orderAmount: 8492},
+      {date: '2021-5-06', orderCount: 60, orderAmount: 6293},
+      {date: '2021-5-07', orderCount: 20, orderAmount: 2293},
+      {date: '2021-5-08', orderCount: 60, orderAmount: 6293},
+      {date: '2021-5-09', orderCount: 50, orderAmount: 5293},
+      {date: '2021-5-10', orderCount: 30, orderAmount: 3293},
+      {date: '2021-5-11', orderCount: 20, orderAmount: 2293},
+      {date: '2021-5-12', orderCount: 80, orderAmount: 8293},
+      {date: '2021-5-13', orderCount: 100, orderAmount: 10293},
+      {date: '2021-5-14', orderCount: 10, orderAmount: 1293},
+      {date: '2021-5-15', orderCount: 40, orderAmount: 4293}
     ]
   };
   export default {
